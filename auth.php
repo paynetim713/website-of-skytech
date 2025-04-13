59e5d69497579c19b88558757e1e9493a3f2e9f0
kris <aslm2l1k123@gmail.com>
2025-04-13 15:34:44 +0800

    Update auth.php

--- auth.php
+++ auth.php
@@ -1,5 +1,5 @@
 <?php
-// 避免重复调用 session_start()
+
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
@@ -8,7 +8,7 @@ function isAdmin() {
     return isset($_SESSION['user_level']) && $_SESSION['user_level'] === 'Admin';
 }
 
-// 访问控制
+
 if (!isset($_SESSION['user_id'])) {
     header("Location: login.php");
     exit();
